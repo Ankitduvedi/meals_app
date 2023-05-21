78c7e3c338e5aff6ce2e3625f2ebf672b7298029
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-21 16:20:16 +0530

    initial commit

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -12,17 +12,20 @@ class CategoryGridItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(16),
       child: Container(
         padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(10),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             gradient: LinearGradient(colors: [
               category.color.withOpacity(0.55),
               category.color.withOpacity(0.9),
             ])),
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+        child: Center(
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
         ),
       ),
     );
