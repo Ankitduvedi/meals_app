c6f1259d685768b5128701533842a8555c913cf2
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-24 01:36:12 +0530

    style

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
   final Meal meal;
 
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
@@ -17,6 +29,9 @@ class MealItem extends StatelessWidget {
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
             Positioned(
                 bottom: 0,
@@ -44,7 +59,17 @@ class MealItem extends StatelessWidget {
                       ),
                       const SizedBox(height: 12),
                       Row(
-                        children: [],
+                        children: [
+                          MealItemTrait(
+                              icon: Icons.schedule,
+                              label: '${meal.duration} min'),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          MealItemTrait(
+                              icon: Icons.schedule,
+                              label: '${meal.duration} min'),
+                        ],
                       )
                     ],
                   ),
