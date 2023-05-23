73eeeca8f1173d1862531146b72fbe88912ff360
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-24 02:51:49 +0530

    screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -12,9 +12,13 @@ class MealsScreen extends StatelessWidget {
   final String title;
   final List<Meal> meals;
 
- void selectMeal(BuildContext context,Meal meal) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>MealDetailsScreen(meal: meal),),);
- }
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +50,12 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
           itemCount: meals.length,
-          itemBuilder: (context, index) => MealItem(meal: meals[index],onSelectMeal: ,));
+          itemBuilder: (context, index) => MealItem(
+                meal: meals[index],
+                onSelectMeal: (meal) {
+                  selectMeal(context, meal);
+                },
+              ));
     }
     return Scaffold(
       appBar: AppBar(
