85a785998f458e6a09768ed6a6d0016ee349eaf1
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-30 23:06:47 +0530

    filters

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -11,6 +11,8 @@ class MealDetailsScreen extends ConsumerWidget {
   final Meal meal;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoritMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -29,7 +31,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           )
         ],
       ),
